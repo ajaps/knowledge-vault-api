930b7cfe217801ad49724206d4a7e2242f87964c
Ajaps <ajapsfranklin@gmail.com>
2025-10-08 00:32:49 +0100

    models created

---
+++ db/migrate/20251007232751_create_documents.rb
@@ -0,0 +1,11 @@
+class CreateDocuments < ActiveRecord::Migration[8.0]
+  def change
+    create_table :documents do |t|
+      t.references :vault, null: false, foreign_key: true
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
